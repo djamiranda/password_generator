b0615014cfdc5c779854ee17f31d8c83b12bff68
djamiranda <djamiranda@gmail.com>
2023-05-02 23:30:07 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+var uppercase = []
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
