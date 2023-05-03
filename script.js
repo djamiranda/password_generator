68f78e0367869dcb299f53f755fc19c2e7e58ec0
djamiranda <djamiranda@gmail.com>
2023-05-03 01:14:58 -0400

    finished arrays & added prompts

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var uppercase = []
-var lowercase = []
-var special = []
-var numbers = []
+var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var specialCharacters = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "{", "}", "[", "]", "|", "\", "/", ":", ";", """, "'", "<", ">", ",", ".", "?"];
+var numberCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+var length = prompt ("Enter Password length from 8 to 128 characters");
+var lower = confirm ("Do you want lowercase letters?");
+var uppper = confirm ("Do you want upppercase lettes?");
+var special = confirm ("Do you want special characters?");
+var numbers = confirm ("Do you want numbers?");
+
+
+
+
+
 
 
 // Write password to the #password input
