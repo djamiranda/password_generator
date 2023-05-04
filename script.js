63495247bdad71495e7a6369c1334392cb4efa7c
djamiranda <djamiranda@gmail.com>
2023-05-04 12:34:49 -0400

    added comments, fixed special character array & added generatePassword function

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Added letter & character arrays
 var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var specialCharacters = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "{", "}", "[", "]", "|", "\", "/", ":", ";", """, "'", "<", ">", ",", ".", "?"];
+var specialCharacters = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "{", "}", "[", "]", "|", "<", ">", ","];
 var numberCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
+// Added password length, letter & character prompts
 var length = prompt ("Enter Password length from 8 to 128 characters");
 var lower = confirm ("Do you want lowercase letters?");
 var uppper = confirm ("Do you want upppercase lettes?");
 var special = confirm ("Do you want special characters?");
 var numbers = confirm ("Do you want numbers?");
-
-
-
-
-
 
 
 // Write password to the #password input
@@ -24,6 +21,10 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+}
+
+function generatepPassword() {
 
 }
 
